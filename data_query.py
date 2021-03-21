54f2dd3a7dd7f7c87bd8b533cedc229e9d637239
Zhu Haoxiang <elon.zhu@outlook.com>
2021-03-21 21:08:24 +0800

    2021-03-21 21:09

---
+++ data_query.py
@@ -0,0 +1,48 @@
+import mysql.connector
+import pandas as pd
+
+def connect_info():
+    host_ = "127.0.0.1"
+    user_ = "root"
+    password_ = "nbsbest"
+    return host_, user_, password_
+
+def get_all():
+	mydb = mysql.connector.connect(
+		host = connect_info().host_
+		user = connect_info().user_
+		password = connect_info().password_
+	)
+	mycursor = mydb.cursor(dictionary=True)
+	# get all data
+	mycursor.execute("SELECT * FROM new_schema.mock_data")
+	data = mycursor.fetchall()
+	df_all = pd.DataFrame(data)
+	return df_all
+
+def get_filters(filters_clause, aggFunc, aggCol, start_date, end_date, groupCol):
+      mydb = mysql.connector.connect(
+		host = connect_info().host_
+		user = connect_info().user_
+		password = connect_info().password_
+      )
+      mycursor = mydb.cursor(dictionary=True)
+
+      if filters_count != 0:
+        if (groupCol != "None") & (aggCol != "None") & (aggFunc != "None"):
+          clause = "SELECT {}({}) FROM new_schema.mock_data WHERE {} AND date BETWEEN '{}' AND '{}' GROUP BY {}".format(filters_clause, aggFunc, aggCol, start_date, end_date, groupCol)
+        else:
+          clause = "SELECT * FROM new_schema.mock_data WHERE {} AND date BETWEEN '{}' AND '{}'".format(filters_clause, start_date, end_date)
+
+      else:
+        if (groupCol != "None") & (aggCol != "None") & (aggFunc != "None"):
+          clause = "SELECT {}({}) FROM new_schema.mock_data WHERE date BETWEEN '{}' AND '{}' GROUP BY {}".format(aggFunc, aggCol, start_date, end_date, groupCol)
+        else:
+          clause = "SELECT * FROM new_schema.mock_data WHERE date BETWEEN '{}' AND '{}'".format(start_date, end_date)
+
+      mycursor.execute(clause)
+      data = mycursor.fetchall()
+
+      df_final = pd.DataFrame(data).reset_index()
+
+      return df_final
---
+++ initial.py
@@ -0,0 +1,125 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from interface import Ui_MainWindow
+import data_query
+import sys
+import json
+import mysql.connector
+import pandas as pd
+
+def initialise():
+    df_all = data_query.get_all()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    # set date combo
+    for col in df_all.columns:
+      ui.time_combo.addItem(col)
+    # set filter combo
+    ui.filter_combo.addItem("None")
+    for col in df_all.columns:
+      ui.filter_combo.addItem(col)
+
+    # set operator combo
+    ui.operator_combo.addItem("=")
+    ui.operator_combo.addItem("!=")
+    ui.operator_combo.addItem(">")
+    ui.operator_combo.addItem("<")
+    ui.operator_combo.addItem(">=")
+    ui.operator_combo.addItem("<=")
+
+    # set group by column
+    ui.by_col_combo.addItem("None")
+    for col in df_all.columns:
+      ui.by_col_combo.addItem(col)
+    # set aggregate column
+    ui.agg_col_combo.addItem("None")
+    for col in df_all.columns:
+      ui.agg_col_combo.addItem(col)
+    # set aggregate function
+    ui.agg_func_combo.addItem("None")
+    ui.agg_func_combo.addItem("COUNT")
+    ui.agg_func_combo.addItem("SUM")
+    ui.agg_func_combo.addItem("AVG")
+    ui.agg_func_combo.addItem("MAX")
+    ui.agg_func_combo.addItem("MIN")
+
+    def add_filters(self):
+      oth_filter = self.filter_combo.currentText()
+      operator = self.operator_combo.currentText()
+      comparison = self.compare_text.toPlainText()
+      if oth_filter != "None":
+        self.filter_list.addItem(oth_filter + operator + comparison)
+
+    def clear_filters(self):
+      self.filter_list.takeItem(self.filter_list.row(self.filter_list.selectedItems()[0]))
+
+    def querys(self):
+      date_col = self.time_combo.currentText()
+      start_date = self.from_combo.date().toString("yyyy-MM-dd")
+      end_date = self.to_combo.date().toString("yyyy-MM-dd")
+      groupCol = self.by_col_combo.currentText()
+      aggCol = self.agg_col_combo.currentText()
+      aggFunc = self.agg_func_combo.currentText()
+      filters_count = self.filter_list.count()
+      filters_items = [self.filter_list.item(i).text() for i in range(filters_count)]
+
+      filters_clause = " AND ".join(filters_items)
+
+      global df_query
+
+      df_query = get_filters(filters_clause, aggFunc, aggCol, start_date, end_date, groupCol)
+      print(df_query)
+
+    def exports(self):
+      df_query.to_csv("data.csv")
+
+    def records(self):
+      date_col = self.time_combo.currentText()
+      start_date = self.from_combo.date().toString("yyyy-MM-dd")
+      end_date = self.to_combo.date().toString("yyyy-MM-dd")
+      groupCol = self.by_col_combo.currentText()
+      aggCol = self.agg_col_combo.currentText()
+      aggFunc = self.agg_func_combo.currentText()
+      filters_count = self.filter_list.count()
+      filters_items = [self.filter_list.item(i).text() for i in range(filters_count)]
+
+      record_dict = {
+        "date_col": date_col,
+        "start_date": start_date,
+        "end_date": end_date,
+        "groupCol": groupCol,
+        "aggCol": aggCol,
+        "aggFunc": aggFunc,
+        "filters": filters_items
+      }
+
+      with open('record.txt', 'w') as json_file:
+        json.dump(record_dict, json_file)
+
+    ui.add_filter = add_filters.__get__(ui)
+    ui.clear_filter = clear_filters.__get__(ui)
+    ui.query = querys.__get__(ui)
+    ui.export = exports.__get__(ui)
+    ui.record = records.__get__(ui)
+
+    # add button
+    ui.add_button.clicked.connect(ui.add_filter)
+    # clear button
+    ui.clear_button.clicked.connect(ui.clear_filter)
+    # query button
+    ui.query_button.clicked.connect(ui.query)
+    # export button
+    ui.export_button.clicked.connect(ui.export)
+    # record button
+    ui.record_button.clicked.connect(ui.record)
+
+
+
+
+
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+initialise()
