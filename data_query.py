a6522b97cf2db3cbd38a8ba612ee52f3c48a4991
Zhu Haoxiang <elon.zhu@outlook.com>
2021-03-21 21:28:59 +0800

    2021-03-21 21:28

--- data_query.py
+++ data_query.py
@@ -9,9 +9,9 @@ def connect_info():
 
 def get_all():
 	mydb = mysql.connector.connect(
-		host = connect_info().host_
-		user = connect_info().user_
-		password = connect_info().password_
+		host = connect_info()[0],
+		user = connect_info()[1],
+		password = connect_info()[2]
 	)
 	mycursor = mydb.cursor(dictionary=True)
 	# get all data
@@ -21,28 +21,28 @@ def get_all():
 	return df_all
 
 def get_filters(filters_clause, aggFunc, aggCol, start_date, end_date, groupCol):
-      mydb = mysql.connector.connect(
-		host = connect_info().host_
-		user = connect_info().user_
-		password = connect_info().password_
-      )
-      mycursor = mydb.cursor(dictionary=True)
+    mydb = mysql.connector.connect(
+		host = connect_info()[0],
+		user = connect_info()[1],
+		password = connect_info()[2]
+        )
+    mycursor = mydb.cursor(dictionary=True)
 
-      if filters_count != 0:
+    if filters_count != 0:
         if (groupCol != "None") & (aggCol != "None") & (aggFunc != "None"):
-          clause = "SELECT {}({}) FROM new_schema.mock_data WHERE {} AND date BETWEEN '{}' AND '{}' GROUP BY {}".format(filters_clause, aggFunc, aggCol, start_date, end_date, groupCol)
+            clause = "SELECT {}({}) FROM new_schema.mock_data WHERE {} AND date BETWEEN '{}' AND '{}' GROUP BY {}".format(filters_clause, aggFunc, aggCol, start_date, end_date, groupCol)
         else:
-          clause = "SELECT * FROM new_schema.mock_data WHERE {} AND date BETWEEN '{}' AND '{}'".format(filters_clause, start_date, end_date)
+            clause = "SELECT * FROM new_schema.mock_data WHERE {} AND date BETWEEN '{}' AND '{}'".format(filters_clause, start_date, end_date)
 
-      else:
+    else:
         if (groupCol != "None") & (aggCol != "None") & (aggFunc != "None"):
-          clause = "SELECT {}({}) FROM new_schema.mock_data WHERE date BETWEEN '{}' AND '{}' GROUP BY {}".format(aggFunc, aggCol, start_date, end_date, groupCol)
+            clause = "SELECT {}({}) FROM new_schema.mock_data WHERE date BETWEEN '{}' AND '{}' GROUP BY {}".format(aggFunc, aggCol, start_date, end_date, groupCol)
         else:
-          clause = "SELECT * FROM new_schema.mock_data WHERE date BETWEEN '{}' AND '{}'".format(start_date, end_date)
+            clause = "SELECT * FROM new_schema.mock_data WHERE date BETWEEN '{}' AND '{}'".format(start_date, end_date)
 
-      mycursor.execute(clause)
-      data = mycursor.fetchall()
+    mycursor.execute(clause)
+    data = mycursor.fetchall()
 
-      df_final = pd.DataFrame(data).reset_index()
+    df_final = pd.DataFrame(data).reset_index()
 
-      return df_final
+    return df_final
